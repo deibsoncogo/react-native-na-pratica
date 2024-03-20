1292e8935cdc28aeac926173cdfc07bcf7b235e8
Deibson Cogo <deibsoncogo@outlook.com>
2024-03-19 21:59:14 -0300

    ✨ feat: criação de novos serviços

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -1,6 +1,13 @@
 import * as ingredients from "./ingredients";
+import * as preparations from "./preparations";
+import * as recipes from "./recipes";
 
 export const services = {
   ingredients,
-  storage: { imagePath: process.env.EXPO_PUBLIC_SUPABASE_BUCKET_INGREDIENTS },
+  recipes,
+  preparations,
+
+  storage: {
+    imagePath: process.env.EXPO_PUBLIC_SUPABASE_BUCKET_INGREDIENTS,
+  },
 };
