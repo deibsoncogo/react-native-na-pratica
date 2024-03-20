cf563b9450856430f4d6a6ada70c5e66835e8533
Deibson Cogo <deibsoncogo@outlook.com>
2024-03-19 22:28:38 -0300

    ✨ feat: criação final da página recipes

--- src/app/recipes/styles.ts
+++ src/app/recipes/styles.ts
@@ -1,9 +1,9 @@
-import { StyleSheet } from "react-native";
-import { theme } from "../../theme";
+import { StyleSheet } from "react-native"
+import { theme } from "../../theme"
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
 
   header: {
@@ -16,5 +16,19 @@ export const styles = StyleSheet.create({
     fontSize: theme.fonts.size.heading.md,
     fontFamily: theme.fonts.family.bold,
     marginTop: 22,
+  },
+
+  recipes: {
+    padding: 32,
+  },
+
+  recipesContent: {
+    gap: 16,
+  },
+
+  empty: {
+    fontSize: theme.fonts.size.body.md,
+    fontFamily: theme.fonts.family.regular,
+    color: theme.colors.gray_400,
   },
 })
